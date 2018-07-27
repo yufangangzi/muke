c7e8abdc92cfb6e62a5e5876d7b0c2d6304c12d9
陈刚 <769063757@qq.com>
2018-07-27 12:38:27 +0800

    chuangjian

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+const autoprefixer = require('autoprefixer');
+module.exports = {
+  plugins: [
+    autoprefixer()  //需要加浏览器前缀的css
+  ]
+}
+
+// 帮我们优化css 的
---
+++ src/app.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="test">
+    <div id="cover"></div>
+    <Footer></Footer>
+  </div>
+</template>
+<script>
+  import Footer from './todos/footer.jsx'
+  export default {
+    data () {
+      return {
+        text:'abcuuu'
+      }
+    },
+    components:{
+      Footer
+    }
+  }
+</script>
+<style lang="stylus" scoped>
+#test
+  position absolute
+  left:0
+  top:0
+  right:0
+  bottom:0
+  background :url('./assets/images/timg1.jpg') no-repeat
+  #cover
+    position absolute
+    left:0
+    top:0
+    right:0
+    bottom:0
+    z-index :1
+    opacity: 0.9
+    background-color :#999
+</style>
