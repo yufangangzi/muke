427780a70837e7f17b9a0eb7d1eb543a320f0f88
陈刚 <769063757@qq.com>
2018-12-07 14:48:45 +0800

    json

--- client/layout/header.vue
+++ client/layout/header.vue
@@ -9,19 +9,7 @@
           @select="handleSelect"
           active-text-color="#ffd04b">
           <el-menu-item index="1">处理中心</el-menu-item>
-          <el-submenu index="2">
-            <template slot="title">我的工作台</template>
-            <el-menu-item index="2-1">选项1</el-menu-item>
-            <el-menu-item index="2-2">选项2</el-menu-item>
-            <el-menu-item index="2-3">选项3</el-menu-item>
-            <el-submenu index="2-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="2-4-1">选项1</el-menu-item>
-              <el-menu-item index="2-4-2">选项2</el-menu-item>
-              <el-menu-item index="2-4-3">选项3</el-menu-item>
-            </el-submenu>
-          </el-submenu>
-          <el-menu-item index="3">消息中心</el-menu-item>
+          <el-menu-item index="work">工作台</el-menu-item>
           <el-menu-item index="family">个人信息</el-menu-item>
           <el-menu-item index="sing">唱歌</el-menu-item>
           <el-menu-item index="video">视频</el-menu-item>
